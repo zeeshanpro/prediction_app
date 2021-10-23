ef267d3f00a37d531c00159eed2418b7532fea87
OmerAnwaar <32648852+zeeshanpro@users.noreply.github.com>
2021-10-24 03:03:19 +0500

    Home dashboard stats

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Prediction;
+use App\Models\Game;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +26,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home_v2');
+        $openGames		    = count(Game::where(['is_status' => 1])->get());
+        $completedGames		= count(Game::where(['is_status' => 2])->get());
+        $users		        = count(User::where(['user_type' => 2])->get());
+        $data['openGames']  = $openGames;    
+        $data['completedGames']   = $completedGames;    
+        $data['users']   = $users;  
+
+        return view('home_v2',$data);
     }
 }
